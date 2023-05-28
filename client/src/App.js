93998e5681dfd1bc95e43d17e4dc33ade0906557
bitofcoding <92008241+bitofcoding@users.noreply.github.com>
2023-05-29 04:58:38 +0530

    switch chains

--- client/src/App.js
+++ client/src/App.js
@@ -18,11 +18,19 @@ function App() {
   const loadWeb3 = async () => {
     if (window.ethereum) {
       window.web3 = new Web3(window.ethereum);
+			await window.ethereum.request({
+				method: 'wallet_switchEthereumChain',
+				params: [{ chainId: '0x539' }], // chainId must be in hexadecimal numbers
+			});
       await window.ethereum.request({
         method: "eth_requestAccounts",
       });
     } else if (window.web3) {
       window.web3 = new Web3(window.web3.currentProvider);
+			await window.ethereum.request({
+				method: 'wallet_switchEthereumChain',
+				params: [{ chainId: '0x61' }], // chainId must be in hexadecimal numbers
+			});
     } else {
       window.alert("Non Ethereum browser detected");
     }
