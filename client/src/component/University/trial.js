9b24b17b141c201725aa03e486cdc8191e9d96ff
bitofcoding <92008241+bitofcoding@users.noreply.github.com>
2023-07-13 12:18:19 +0530

    dont know kya change kiya mein

--- client/src/component/University/trial.js
+++ client/src/component/University/trial.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { connect } from "./../../functions";
+import { connect } from "../../functions";
 import { useDispatch } from "react-redux";
 import { UniversitySignIn, UniversitySignUp } from "../../actions/auth";
 import { useNavigate } from "react-router-dom";
